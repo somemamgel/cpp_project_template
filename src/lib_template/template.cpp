bc1a0f6179fe9069cdc973d51bbfbf9eea587435
somemamgel <somemamgel@foxmail.com>
2024-01-03 00:47:26 +0800

    init

---
+++ src/lib_template/template.cpp
@@ -0,0 +1,6 @@
+#include "lib_template/template.h"
+
+int hello()
+{
+        return 2;
+}
---
+++ tests/unit_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+#include "lib_template/template.h"
+
+TEST(test_suite_name, hello) {
+    ASSERT_EQ(2, hello());
+}
